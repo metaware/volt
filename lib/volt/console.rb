d6f9e77dcbb15085be7ad23c2d452f066b2fa480
Ryan Stout <ryanstout@gmail.com>
2014-06-02 00:19:09 -0600

    have pry trigger events after each line

--- lib/volt/console.rb
+++ lib/volt/console.rb
@@ -1,29 +1,48 @@
+require 'pry'
+
+class Pry
+  # To make the console more useful, we make it so we flush the event registry
+  # after each line.  This makes it so events are triggered after each line.
+  # To accomplish this we monkey-patch pry.
+  def rep(target=TOPLEVEL_BINDING)
+    target = Pry.binding_for(target)
+    result = re(target)
+
+    Pry.critical_section do
+      show_result(result)
+    end
+
+    $event_registry.flush!
+  end
+end
+
 class Console
   def self.start
-    require 'pry'
 
     $LOAD_PATH << 'lib'
     ENV['SERVER'] = 'true'
 
-    require 'volt'
-    require 'volt/models'
-    require 'volt'
-    require 'volt/page/page'
-    require 'volt/server/rack/component_paths'
-    require 'volt/server/socket_connection_handler_stub'
+    require 'volt/reactive/reactive_value'
+    # require 'volt'
+    # require 'volt/models'
+    # require 'volt/page/page'
+    # require 'volt/server/rack/component_paths'
+    # require 'volt/server/socket_connection_handler_stub'
+    #
+    # SocketConnectionHandlerStub.dispatcher = Dispatcher.new
+    #
+    #
+    # app_path = File.expand_path(File.join(Dir.pwd, "app"))
+    # component_paths = ComponentPaths.new
+    # component_paths.require_in_components
 
-    SocketConnectionHandlerStub.dispatcher = Dispatcher.new
-
-
-    app_path = File.expand_path(File.join(Dir.pwd, "app"))
-    component_paths = ComponentPaths.new
-    component_paths.require_in_components
+    $event_registry = EventRegistry.new
 
     Pry.config.prompt_name = 'volt'
 
     # start a REPL session
-    # Pry.start
+    Pry.start
 
-    Page.new.pry
+    # Page.new.pry
   end
 end
